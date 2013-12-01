c3f79bbcadc918bbedb362e57fec5108a2057203
QPhong <QPhong@QPhongs-MacBook-Air.local>
2013-12-01 17:17:05 +0800

    optimize python code

--- large_rockSample/RS_main.py
+++ large_rockSample/RS_main.py
@@ -17,69 +17,122 @@ def makePolicyDict(rs_Mmdp, policyList):
 
 	return policy
 
+mdp_numStates = 0
+mapStates = {}
+mdp_trans_act = []
+mdp_trans_nxtS = []
+mdp_trans_p = []
+mdp_rewards = []
+mdp_rb = []
+
+def initIndexingModel(rs_Mmdp):
+
+	global mdp_numStates
+	global mapStates
+	global mdp_trans_act
+	global mdp_trans_nxtS
+	global mdp_trans_p
+	global mdp_rewards
+	global mdp_rb
+
+	mdp_numStates = len(rs_Mmdp.states)
+
+	c = 0
+	mapStates = {}
+	for s in rs_Mmdp.states:
+		mapStates[s] = c
+		c += 1	
+
+	tmp = []
+	for i in range(c):
+		tmp.append( mdpSlvi.vec1s(rs_Mmdp.adj[ rs_Mmdp.states[i] ].keys()) )
+	mdp_trans_act = mdpSlvi.vec2s(tmp)
+	
+	tmp = []
+	for i in range(c):
+		tmp1 = []
+		s = rs_Mmdp.states[i]
+		for a in rs_Mmdp.adj[s]:
+			tmp2 = []
+			for nxtS in rs_Mmdp.adj[s][a]:
+				tmp2.append( mapStates[nxtS] )
+			tmp1.append( mdpSlvi.vec1i(tmp2) )
+		tmp.append( mdpSlvi.vec2i(tmp1) )
+	mdp_trans_nxtS = mdpSlvi.vec3i(tmp)
+
+	mdp_trans_p = []
+	for i in range(c):
+		tmp1 = []
+		s = rs_Mmdp.states[i]
+		for a in rs_Mmdp.adj[s]:
+			tmp2 = []
+			for nxtS in rs_Mmdp.adj[s][a]:
+				tmp2.append( 0.0 )
+			tmp1.append( tmp2 )
+		mdp_trans_p.append( tmp1 ) 
+
+	mdp_rewards = []
+	for i in range(c):
+		tmp1 = []
+		s = rs_Mmdp.states[i]
+		for a in rs_Mmdp.adj[s]:
+			tmp1.append( 0.0 )
+		mdp_rewards.append( tmp1 )
+
+	mdp_rb = []
+	for i in range(c):
+		tmp1 = []
+		s = rs_Mmdp.states[i]
+		for a in rs_Mmdp.adj[s]:
+			tmp1.append( 0.0 )
+		mdp_rb.append( tmp1 )
+
 
 def makeIndexingModel(rs_Mmdp):
 
-		mdp_numStates = len(rs_Mmdp.states)
+	global mdp_numStates
+	global mapStates
+	global mdp_trans_act
+	global mdp_trans_nxtS
+	global mdp_trans_p
+	global mdp_rewards
+	global mdp_rb
 
-		c = 0
-		mapStates = {}
-		for s in rs_Mmdp.states:
-			mapStates[s] = c
-			c += 1
+	for i in range(mdp_numStates):
+		s = rs_Mmdp.states[i]
+		tmpA = 0
+		for a in rs_Mmdp.adj[s]:
+			tmpN = 0
+			for nxtS in rs_Mmdp.adj[s][a]:
+				mdp_trans_p[i][tmpA][tmpN] = rs_Mmdp.trans[s][a][nxtS]
+				tmpN += 1
+			tmpA += 1
 
-		tmp = []
-		for i in range(c):
-			tmp.append( mdpSlvi.vec1s(rs_Mmdp.adj[ rs_Mmdp.states[i] ].keys()) )
-		mdp_trans_act = mdpSlvi.vec2s(tmp)
-		
-		tmp = []
-		for i in range(c):
-			tmp1 = []
-			s = rs_Mmdp.states[i]
-			for a in rs_Mmdp.adj[s]:
-				tmp2 = []
-				for nxtS in rs_Mmdp.adj[s][a]:
-					tmp2.append( mapStates[nxtS] )
-				tmp1.append( mdpSlvi.vec1i(tmp2) )
-			tmp.append( mdpSlvi.vec2i(tmp1) )
-		mdp_trans_nxtS = mdpSlvi.vec3i(tmp)
+	for i in range(mdp_numStates):
+		s = rs_Mmdp.states[i]
+		tmpA = 0
+		for a in rs_Mmdp.adj[s]:
+			mdp_rewards[i][tmpA] = rs_Mmdp.rewards[s][a]
+			tmpA += 1
 
-		tmp = []
-		for i in range(c):
-			tmp1 = []
-			s = rs_Mmdp.states[i]
-			for a in rs_Mmdp.adj[s]:
-				tmp2 = []
-				for nxtS in rs_Mmdp.adj[s][a]:
-					tmp2.append( rs_Mmdp.trans[s][a][nxtS] )
-				tmp1.append( mdpSlvi.vec1d(tmp2) )
-			tmp.append( mdpSlvi.vec2d(tmp1) ) 
-		mdp_trans_p = mdpSlvi.vec3d(tmp)
-
-		tmp = []
-		for i in range(c):
-			tmp1 = []
-			s = rs_Mmdp.states[i]
-			for a in rs_Mmdp.adj[s]:
-				tmp1.append( rs_Mmdp.rewards[s][a] )
-			tmp.append( mdpSlvi.vec1d(tmp1) )
-		mdp_rewards = mdpSlvi.vec2d(tmp)
-
-		tmp = []
-		for i in range(c):
-			tmp1 = []
-			s = rs_Mmdp.states[i]
-			for a in rs_Mmdp.adj[s]:
-				tmp1.append( rs_Mmdp.RB[s][a] )
-			tmp.append( mdpSlvi.vec1d(tmp1) )
-		mdp_rb = mdpSlvi.vec2d(tmp)
-
-		return [mdp_numStates, mdp_trans_act, mdp_trans_nxtS,\
-		mdp_trans_p, mdp_rewards, mdp_rb]
+	for i in range(mdp_numStates):
+		s = rs_Mmdp.states[i]
+		tmpA = 0
+		for a in rs_Mmdp.adj[s]:
+			mdp_rb[i][tmpA] = rs_Mmdp.RB[s][a]
+			tmpA += 1
 
 
 def Simulation(rs_Mmdp):
+
+	global mdp_numStates
+	global mapStates
+	global mdp_trans_act
+	global mdp_trans_nxtS
+	global mdp_trans_p
+	global mdp_rewards
+	global mdp_rb
+
 	#accumulate reward
 	acc_reward=0
 
@@ -98,23 +151,34 @@ def Simulation(rs_Mmdp):
 			rs_Mmdp.calRewards()
 			rs_Mmdp.calRB()
 
-
-		## Python code
-		# m_policy = RS_mdp_solver(rs_Mmdp,dis_RB,discount_fac)
+		curTime = clock()
+		# # Python code
+		m_policy = RS_mdp_solver(rs_Mmdp,dis_RB,discount_fac)
+		print "Python solver", (clock() - curTime), "seconds"
+		curTime = clock()
 
 		# C++ code
-		idxModel = makeIndexingModel(rs_Mmdp)
+		makeIndexingModel(rs_Mmdp)
+		mdp_trans_pVec = mdpSlvi.vec3d(mdp_trans_p)
+		mdp_rewardsVec = mdpSlvi.vec2d(mdp_rewards)
+		mdp_rbVec = mdpSlvi.vec2d(mdp_rb)
+		
+		print "Make model:", (clock() - curTime), "seconds"
+		curTime = clock()
 
-		policyList = mdpSlvi.RS_mdp_solver(idxModel[0], idxModel[1],\
-			idxModel[2], idxModel[3], idxModel[4], idxModel[5],\
+		policyList = mdpSlvi.RS_mdp_solver(mdp_numStates, mdp_trans_act, \
+			mdp_trans_nxtS, mdp_trans_pVec, mdp_rewardsVec,\
+			mdp_rbVec, \
 			rs_Mmdp.H, dis_RB, discount_fac)
 
 		m_policy = makePolicyDict(rs_Mmdp, policyList)
 
+		print "C++ solver", (clock() - curTime), "seconds"
 		# # test implementation Python and C++
 		# for s in rs_Mmdp.states:
 		# 	if m_policy[s] != m_policy1[s]:
 		# 		print s, m_policy[s], m_policy1[s]
+		# print "finish test"
 
 		#execute part
 		[flag,reward]=rs_Mmdp.execution(m_policy)
@@ -172,6 +236,7 @@ def main():
 			rocks = genRocks(j, Num_rocks)
 			print rocks
 			rs_Mmdp=MDP(rocks)
+			initIndexingModel(rs_Mmdp)
 			rw = Simulation(rs_Mmdp)
 			print rw
 			avgReward += rw / 2048.0
